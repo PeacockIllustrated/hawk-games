7af7a972ac5b3a044af6f55f587e5f370bb69484
PeacockIllustrated <138880912+PeacockIllustrated@users.noreply.github.com>
2025-09-08 18:51:30 +0100

    Update firebase-config.js

--- app/js/firebase-config.js
+++ app/js/firebase-config.js
@@ -1,8 +1,9 @@
-const firebaseConfig = {
+// /app/js/firebase-config.js
+export const firebaseConfig = {
   apiKey: "AIzaSyCHnYCOB-Y4tA1_ikShsBZJVD0KJfJJMdU",
   authDomain: "the-hawk-games-64239.firebaseapp.com",
   projectId: "the-hawk-games-64239",
-  storageBucket: "the-hawk-games-64239.firebasestorage.app",
+  storageBucket: "the-hawk-games-64239.firebasestorage.app", // see note below
   messagingSenderId: "391161456812",
   appId: "1:391161456812:web:48f7264720dff9a70dd709",
   measurementId: "G-DGLYCBJLWF"

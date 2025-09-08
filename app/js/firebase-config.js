d2d0056b58dfe7ba49fe7c86cf1efc871ac3c73d
PeacockIllustrated <138880912+PeacockIllustrated@users.noreply.github.com>
2025-09-08 18:46:14 +0100

    Update firebase-config.js

--- app/js/firebase-config.js
+++ app/js/firebase-config.js
@@ -1,11 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCHnYCOB-Y4tA1_ikShsBZJVD0KJfJJMdU",
   authDomain: "the-hawk-games-64239.firebaseapp.com",
@@ -15,7 +7,3 @@ const firebaseConfig = {
   appId: "1:391161456812:web:48f7264720dff9a70dd709",
   measurementId: "G-DGLYCBJLWF"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
